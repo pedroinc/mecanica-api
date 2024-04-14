0331f7c675d6f4719f9b415e9de4c36ff5b65b34
Pedro Henrique de Lacerda <pedroinc@gmail.com>
2024-04-14 01:15:41 -0300

    server.js changes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,44 @@
+require('dotenv').config();
+
 const express = require('express');
 const sequelize = require('./database/index');
-
 const app = express();
+const isTokenValid = require('./middlewares/isTokenValid');
+
+const config = {
+  hostname: '0.0.0.0',
+  port: process.env.PORT,
+};
 
 app.use(express.json());
 
+app.get('/', async (req, res) => {
+  try {
+    return res.json({ message: 'Mecanica API - Home' });
+  } catch (error) {
+    return res.json({ error });
+  }
+});
+
+app.get('/db/init', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequelize.sync({ force: true });
+    console.log('All models were synchronized successfully.');
+    return res.json({ message: 'All models were synchronized successfully.' });
+  } catch (error) {
+    const prefix = 'Unable to connect to the database!';
+    console.error(prefix, error);
+    return res.json({ error: prefix });
+  }
+});
+
 // routes
-app.use('/clientes', require('./routes/customer.routes'));
+app.use('/customers', isTokenValid, require('./routes/customer.routes'));
 
-const port = 3333;
-// console.log(app);
+app.use('/users', require('./routes/user.routes'));
 
-app.listen(port, 'localhost', () => {
-  console.log(`listening on port ${port}`);
+app.listen(config.port, config.hostname, () => {
+  console.log(`server running on port ${config.port}`);
 });

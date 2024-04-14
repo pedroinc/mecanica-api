4af7214dccf47eb68c95ce945b60a9764421b2c0
Pedro Henrique de Lacerda <pedroinc@gmail.com>
2024-04-14 01:08:37 -0300

    added method findByEmail to CustomerRepository

--- src/repositories/CustomerRepository.js
+++ src/repositories/CustomerRepository.js
@@ -2,6 +2,14 @@ const Customer = require("../models/Customer");
 
 class CustomerRepository {
 
+  async findByEmail(email) {
+    return await Customer.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+
   async listAll() {
     return await Customer.findAll({ limit: 10, order: [ ['name', 'asc'] ] });
   }
